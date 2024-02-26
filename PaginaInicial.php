d50fd5e94ecfb2d169731ccaa4eb7ebf9bb250ab
Mariana Görresen <110499531+marigorresen@users.noreply.github.com>
2024-02-25 21:39:38 -0300

    Create PaginaInicial.php

---
+++ PaginaInicial.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Página Inicial</title>
+</head>
+<body>
+<h1>Bem vinfo a Página Inicial</h1>
+<h2>Página de Cadastro de Funcionários (Sample)</h2>
+<button onclick="window.location.href='Employees.php';">Clique aqui</button>
+
+<h2>Página de Cadastro de Produtos</h2>
+<button onclick="window.location.href='Products.php';">Clique aqui</button>
+
+</body>
+</html>
